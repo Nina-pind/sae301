3d0a4fd75c328a284eabd6d543cded799cdc3112
Nina-pind <nina.pinardel@etu.unilim.fr>
2025-10-25 17:42:30 +0200

    final version

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,10 @@ export default defineConfig({
         main: resolve(__dirname, "index.html"),
         parfum: resolve(__dirname, "pages/parfum.html"),
         produit: resolve(__dirname, "pages/produit.html"),
+        sinscrire: resolve(__dirname, "pages/sinscrire.html"),
+        panier: resolve(__dirname, "pages/panier.html"),
+        mentions_légales: resolve(__dirname, "pages/mentions_légales.html"),
+        conditions: resolve(__dirname, "pages/conditions.html"),
 
       },
     },
